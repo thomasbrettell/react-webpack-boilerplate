4bdf43599ee100bcaf4c8c8815f45cfe131e411f
thomasbrettell <thomas.a.brettell@gmail.com>
2021-11-26 18:00:24 +1100

    add: set public path to '/'
    this adds a '/' to the inserted js which prevents 404

--- webpack.common.js
+++ webpack.common.js
@@ -22,6 +22,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './build'),
     filename: 'bundle.[contenthash].js',
+    publicPath: '/',
   },
   optimization: {
     splitChunks: {
